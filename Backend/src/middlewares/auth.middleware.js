64e2550967004acb04866e52583397a33a90e43d
hisamc91237 <hisamc2000@gmail.com>
2026-07-31 20:14:42 +0530

    Redis connected to server

--- Backend/src/middlewares/auth.middleware.js
+++ Backend/src/middlewares/auth.middleware.js
@@ -1,6 +1,7 @@
 const blacklistModel = require("../models/blacklist.model");
 const userModel = require("../models/user.model");
 const jwt = require("jsonwebtoken");
+const redis = require("../config/cache");
 
 const authUser = async (req, res, next) => {
   const token = req.cookies.token;
